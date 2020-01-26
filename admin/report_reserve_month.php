08c471a9a1d3edb77903c8eafb3b1c63c6635157
unknown <tiees288@gmail.com>
2020-01-26 20:25:57 +0700

    2

--- admin/report_reserve_month.php
+++ admin/report_reserve_month.php
@@ -19,3 +19,14 @@ include("../conf/connection.php");
 $month  = $_POST['month'];
 $year    = $_POST['year'];
 ?>
+
+<head>
+    <title>รายงานการจองรประจำเดือน <?= fullmonth($month); ?> พ.ศ. <?= $year + 543 ?> | Food Order System</title>
+</head>
+
+<body>
+    <div class="container" style="padding-top:20px; width:1350px;">
+        <h3 class="text-center">รายงานการจองประจำเดือน</h3>
+        <h3 class="text-center">เดือน <?= fullmonth($month) ?> พ.ศ. <?= $year + 543 ?></h3>
+        <br>
+    </div>
