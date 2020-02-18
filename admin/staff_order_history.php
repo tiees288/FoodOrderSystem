28ae30aa2d36aaaef08aa554781e6b0dc876d9c1
unknown <tiees288@gmail.com>
2020-02-18 10:51:24 +0700

    Change Column size

--- admin/staff_order_history.php
+++ admin/staff_order_history.php
@@ -121,7 +121,7 @@
                                 default:
                                     echo "Error";
                         } ?>
-                        <tr height="49px;">
+                        <tr height="50px;">
                             <td align="center"><a href="order_detailed.php?oid=<?= $result['orderid'] ?>"><?= $result["orderid"]; ?></a></td>
                             <td align="center"> <?= dt_tothaiyear($result['orderdate']) ?></td>
                             <td><?= $cus_data['cus_name'] ?></td>
