6fd3df85dff2d95d454100b9e94e020c5c9a0c4c
unknown <tiees288@gmail.com>
2020-02-21 20:31:30 +0700

    แก้ไข การใส่ตะกร้าชำระ

--- admin/staff_add_list_pay.php
+++ admin/staff_add_list_pay.php
@@ -1,21 +1,21 @@
 <?php
 
-    if (!isset($_GET['oid'])) {
-        echo "<script>window.location.assign('index.php') </script>";
-        exit();
-    }
+if (!isset($_GET['oid'])) {
+    echo "<script>window.location.assign('index.php') </script>";
+    exit();
+}
 
-    if (!isset($_SESSION)) {  // Check if sessio nalready start
-        session_start();
-    }
+if (!isset($_SESSION)) {  // Check if sessio nalready start
+    session_start();
+}
 
-    if (!isset($_SESSION['staff'])) {
-        echo  "<script> alert('กรุณาเข้าสู่ระบบ');window.location.assign('index.php') </script>";
-        exit();
-    }
+if (!isset($_SESSION['staff'])) {
+    echo  "<script> alert('กรุณาเข้าสู่ระบบ');window.location.assign('index.php') </script>";
+    exit();
+}
 
-    require_once('../conf/connection.php');
-    /*
+require_once('../conf/connection.php');
+/*
         $sql_chk 		=	"SELECT * FROM customers WHERE cusid = ".$_SESSION['user_id'];
         $data_cus		=	mysqli_query($link,$sql_chk);
         $result_cus		=	mysqli_fetch_assoc($data_cus);
@@ -25,39 +25,38 @@
             exit();
         }*/
 
-    $sql_order	    =	"SELECT * FROM orders WHERE orderid = '" . $_GET['oid'] . "' ";
-    $data_order     =	mysqli_query($link, $sql_order);
-    $result_order	=	mysqli_fetch_assoc($data_order);
+$sql_order        =    "SELECT * FROM orders WHERE orderid = '" . $_GET['oid'] . "' ";
+$data_order     =    mysqli_query($link, $sql_order);
+$result_order    =    mysqli_fetch_assoc($data_order);
 
-    if (isset($_SESSION['food_admin']['payment'])) {
-        if ($result_order['order_type'] == 2 ) {
+if (!isset($_SESSION['food_admin']['payment'])) {
+    $_SESSION['food_admin']['payment']['orderid']['0'] = $result_order['orderid'];
+    //$_SESSION['food_admin']['payment']['amount']['0'] = 1; // จำนวนสินค้า
+    //$_SESSION['food_admin']['payment']['seats']['0'] = $result_order['order_seats'];
+} else {
+    $key = array_search($_GET['oid'], $_SESSION['food_admin']['payment']['orderid']);
+
+    if ((string) $key != "") {
+        echo "<script> window.location.assign('staff_cart_payment.php'); </script>";
+        exit();
+    } else {
+
+        if ($result_order['order_type'] == 2) {
             echo "<script>alert('ไม่สามารถเลือกรายการนี้ได้'); window.history.back();</script>";
             exit();
         } else {
-            $sql_type = "SELECT order_type FROM orders WHERE orderid = '". $_SESSION['food_admin']['payment']['orderid'][0] ."'";
+            $sql_type = "SELECT order_type FROM orders WHERE orderid = '" . $_SESSION['food_admin']['payment']['orderid'][0] . "'";
             $query_type = mysqli_query($link, $sql_type) or die(mysqli_error($link));
             $result_type = mysqli_fetch_assoc($query_type);
 
-           if ($result_type['order_type'] == 2) { // เช็คเมื่อหยิบ เข้าตะกร้ามากกว่า 1
+            if ($result_type['order_type'] == 2) { // เช็คเมื่อหยิบ เข้าตะกร้ามากกว่า 1
                 echo "<script>alert('ไม่สามารถเลือกรายการนี้ได้'); window.history.back();</script>";
                 exit();
             }
         }
+        // เพิ่ม การสั่งในตะกร้าชำระ
+        $count = count($_SESSION['food_admin']['payment']['orderid']);
+        $_SESSION['food_admin']['payment']['orderid'][$count] = $result_order['orderid'];
     }
-  
-    if (!isset($_SESSION['food_admin']['payment'])) {
-        $_SESSION['food_admin']['payment']['orderid']['0'] = $result_order['orderid'];
-        //$_SESSION['food_admin']['payment']['amount']['0'] = 1; // จำนวนสินค้า
-        //$_SESSION['food_admin']['payment']['seats']['0'] = $result_order['order_seats'];
-    } else {
-        $key = array_search($_GET['oid'], $_SESSION['food_admin']['payment']['orderid']);
-
-        if ((string) $key != "") {
-            echo "<script> alert('รหัสการสั่งนี้มีอยู่ในตะกร้าแล้ว'); window.history.back(); </script>";
-            exit();
-        } else {
-            $count = count($_SESSION['food_admin']['payment']['orderid']);
-            $_SESSION['food_admin']['payment']['orderid'][$count] = $result_order['orderid'];
-        }
-    }
-    echo  "<script>window.location.assign('staff_cart_payment.php') </script>";
+}
+echo  "<script>window.location.assign('staff_cart_payment.php') </script>";
