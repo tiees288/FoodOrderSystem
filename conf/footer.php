aa2c0a435a4a5ae334526729de09cd9065eee07d
unknown <tiees288@gmail.com>
2020-04-21 14:00:43 +0700

    Add Chrome Icon

--- conf/footer.php
+++ conf/footer.php
@@ -2,6 +2,6 @@
   <footer class="footer" style="position: relative; border-top:1px solid #dfe3e8; border-bottom:1px solid #dfe3e8; height:55px; padding: 0.5rem; width: 100%; bottom: 0; background: #FBFBFB">
 
     <div class="text-center darken-grey-text mb-4" style=" margin-top:10px; color:gray;">
-      © <?= date("Y"); ?> Copyright: ระบบขายอาหารตามสั่ง | Best view in Google Chrome
+      © <?= date("Y"); ?> Copyright: ระบบขายอาหารตามสั่ง | Best view in Google Chrome <i class="fa fa-chrome" aria-hidden="true"></i>
     </div>
   </footer>
